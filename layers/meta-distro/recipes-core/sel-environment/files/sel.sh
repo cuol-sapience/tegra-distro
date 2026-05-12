afbb9a2dd9df092c6457491d1de8cb1b3c02fa27
James Cahill <jcahill@ieee.org>
2026-05-12 13:53:46 +0100

    chore/sel-env: source in all shells

--- layers/meta-distro/recipes-core/sel-environment/files/sel.sh
+++ layers/meta-distro/recipes-core/sel-environment/files/sel.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 
-# Set env vars
 export ROS_DISTRO=humble
 
-# Set aliases
-alias sel-switch-desktop="sudo systemctl isolate graphical.target"
-alias sel-switch-cli="sudo systemctl isolate multi-user.target"
-
-# Auto-source ROS setup for interactive shells
-if [ -n "$PS1" ]; then
-    # Try ros-workspace package script first
-    if [ -f /usr/bin/ros_setup.sh ]; then
-        . /usr/bin/ros_setup.sh
-    # falling back to DISTRO
-    elif [ -f /opt/ros/${ROS_DISTRO}/setup.sh ]; then
-        . /opt/ros/${ROS_DISTRO}/setup.sh
-    elif [ -f /opt/ros/${ROS_DISTRO}/setup.bash ]; then
-        . /opt/ros/${ROS_DISTRO}/setup.bash
-    fi
+# Source ROS setup for all shells (login, SSH, interactive, non-interactive)
+if [ -f /usr/bin/ros_setup.sh ]; then
+    . /usr/bin/ros_setup.sh
+elif [ -f /opt/ros/${ROS_DISTRO}/setup.sh ]; then
+    . /opt/ros/${ROS_DISTRO}/setup.sh
+elif [ -f /opt/ros/${ROS_DISTRO}/setup.bash ]; then
+    . /opt/ros/${ROS_DISTRO}/setup.bash
 fi
+
+# Aliases only for interactive shells
+case $- in
+    *i*)
+        alias sel-switch-desktop="sudo systemctl isolate graphical.target"
+        alias sel-switch-cli="sudo systemctl isolate multi-user.target"
+        ;;
+esac
